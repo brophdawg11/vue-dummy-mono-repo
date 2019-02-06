c40414f0aca0b2c9dc3bb68500d9975d48b628bf
Matt Brophy <matt@brophy.org>
2019-02-05 21:07:47 -0500

    Set up unit testing

---
+++ packages/vue-dummy-published-component/test/DummyComponent.spec.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+import DummyComponent from '../dist/dummy-component.umd';
+
+describe('DummyComponent', () => {
+
+    it('Should render "Hello World"', () => {
+        const cmp = mount(DummyComponent)
+        expect(cmp.find('h1').text()).toBe('Hello World!');
+    });
+
+});
